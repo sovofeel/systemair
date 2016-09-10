d8ab7389c375a80005b3a93118ca3747a3e9b2be
Александр <forjob159@gmail.com>
2016-09-10 14:35:24 +0500

    Адаптив продукта

--- dev/js/tabs.js
+++ dev/js/tabs.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 const initTabs = function() {
   const $tabs = $('.js-tabs'),
         activeClass = 'is-active';
+  let tabsCount = 0;
 
   $tabs.each(function(){
     const $this = $(this),
@@ -13,6 +14,19 @@ const initTabs = function() {
     const activeStart = $this.find('.js-tabs-tab.'+activeClass).index();
     $links.removeClass(activeClass);
     $this.find('.js-tabs-link:eq('+activeStart+')').addClass(activeClass);
+
+    $tab.each(function(){
+      const $tb = $(this);
+      $tb.attr('data-index', tabsCount);
+      tabsCount++;
+    });
+
+    $tab.each(function(){
+      const $tb = $(this);
+      let idx = $tb.attr('data-index');
+      let text = $this.find('.js-tabs-link:eq('+idx+')').text();
+      $tb.prepend('<div class="tabs-mobileHeadline"><div class="tabs-bg"></div>'+text+'</div>');
+    });
 
 
     $links.on('click', function(e){
@@ -24,6 +38,13 @@ const initTabs = function() {
       $tab.removeClass(activeClass)
       $this.find('.js-tabs-tab:eq('+idx+')').addClass(activeClass);
     });
+
+
+    $('.tabs-mobileHeadline').on('click', function(e){
+      e.preventDefault();
+      $(this).closest('.js-tabs-tab').toggleClass('is-expanded');
+    });
+
   });
 };
 
